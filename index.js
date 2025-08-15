9c2ceebe70f01ac9dd19710c6d5631f538bc33bf
Batdul <09.abdullah.bhatti@gmail.com>
2025-08-15 19:57:53 -0400

    Update index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+//responsive navbar
+function myFunction() {
+  var x = document.getElementById("myNav");
+  if (x.className === "myNav") {
+    x.className += " responsive";
+  } else {
+    x.className = "myNav";
+  }
+}
